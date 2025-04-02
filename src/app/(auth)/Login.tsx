f904e66c06d441bf2433feef2a5d67fbfc90baf6
KushShah02 <137054982+KushShah01@users.noreply.github.com>
2025-04-03 00:01:31 +0530

    new changes superman

--- src/app/(auth)/Login.tsx
+++ src/app/(auth)/Login.tsx
@@ -1,4 +1,3 @@
-// pages/login.js
 "use client";
 
 import Image from "next/image";
@@ -8,37 +7,22 @@ import { IoEye } from "react-icons/io5";
 import { IoMdEyeOff } from "react-icons/io";
 import { useFormik } from "formik";
 import { LoginSchema } from "@/_components/Validation";
-import InputAdornment from "@mui/material/InputAdornment";
-import IconButton from "@mui/material/IconButton";
-import { TextField } from "@mui/material";
+import { TextField, InputAdornment, IconButton } from "@mui/material";
 import { textFieldStyles } from "@/_components/textFieldStyles";
 import styles from "@/styles/login.module.css";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
+
 const Page = () => {
   const [eye, setEye] = useState(true);
-
-  // SEE PASSWORD
-  const handleClick = () => {
-    setEye(!eye);
-  };
-
-  // FOR INPUT TEXT
-  const [input, setInput] = useState({
-    email: "",
-    password: "",
-  });
-
-  const [loading, setLoading] = useState(true);
   const router = useRouter();
-  // Formik hook
+
+  const handleClick = () => setEye(!eye);
+
   const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
     useFormik({
-      initialValues: {
-        email: input.email,
-        password: input.password,
-      },
+      initialValues: { email: "", password: "" },
       validationSchema: LoginSchema,
       onSubmit: async (values) => {
         try {
@@ -46,21 +30,17 @@ const Page = () => {
             "http://192.168.2.181:3000/admin/login",
             values,
             {
-              headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-              },
+              headers: { "Content-Type": "application/x-www-form-urlencoded" },
             }
           );
-          // console.log("Signin values", await res.data);
           toast.success(res?.data?.data?.msg);
-          const data = await res?.data?.data;
-          console.log("data", data);
+          const data = res?.data?.data;
           localStorage.setItem("loginuser", JSON.stringify(data?.email));
           localStorage.setItem("userName", JSON.stringify(data?.name));
           localStorage.setItem("loginjwt", data?.token);
-          // toast.error(res?.data?.message);
+          localStorage.setItem("refreshjwt", data?.refreshtoken);
 
-          router.push('/dashboard')
+          router.push("/dashboard");
         } catch (error) {
           console.log("error", error);
           toast.error(error?.message);
@@ -69,56 +49,51 @@ const Page = () => {
     });
 
   return (
-    <div className="flex min-h-screen bg-[url(/background.png)] bg-cover flex-col justify-between gap-4 px-4 py-6">
+    <div className="relative flex min-h-screen bg-[url(/background.png)] bg-cover flex-col justify-between gap-4 px-4 py-6">
       {/* FIRST IMAGE */}
-      <div className="firstimage ">
+      <div className={`${styles.firstImage}`}>
         <Image
           src="/l2.png"
           width={491}
-          // className={`w-[50%] absolute bottom-0 left-0 sm:w-[40%] md:w-[35%] lg:w-[33%] ${styles.loginImage}`}
-          className={` ${styles.loginImage} absolute w-[32%] bottom-0 left-0 sm:w-[40%] md:w-[45%] lg:w-[33%]`}
           height={314}
+          className={`${styles.loginImage}`}
           alt="l2"
         />
       </div>
 
       {/* MAIN CONTENT */}
-      <div className="flex flex-col items-center w-full">
+      <div className="flex flex-col items-center w-full z-10">
         <div
-          className={`flex bg-white shadow-2xl py-10 flex-col items-center text-black w-full sm:w-[80%] md:w-[60%] lg:w-[40%] xl:w-[33%] 2xl:w-[30%] ${styles.content}`}
+          className={`${styles.content} bg-white shadow-2xl flex flex-col items-center text-black`}
         >
           <Image
             src="/logo.png"
-            className={` ${styles.logo} lg:w-[40%] md:w-[30%]`}
+            className={`${styles.logo}`}
             width={250}
             height={60}
             alt="logo"
           />
-
-          <div className="logo flex mt-8 items-center justify-center">
+          <div className="flex mt-8 items-center justify-center">
             <Image
               src="/l1.png"
-              className={` ${styles.login}   md:w-[40%] lg:w-[40%]`}
+              className={`${styles.login}`}
               width={215}
               height={115}
               alt="login"
             />
           </div>
 
-          <div className="div mt-12 w-full max-w-md px-4">
-            <form action="" onSubmit={handleSubmit}>
+          <div className="w-full max-w-md px-4 mt-12">
+            <form onSubmit={handleSubmit}>
               <div className="flex flex-col gap-8">
                 <div>
                   <div className="flex items-center gap-2">
-                    <div className="mt-3">
-                      <Image
-                        src="/email.png"
-                        alt="Email Icon"
-                        width={16}
-                        height={20}
-                        className="shrink-0"
-                      />
-                    </div>
+                    <Image
+                      src="/email.png"
+                      alt="Email Icon"
+                      width={16}
+                      height={20}
+                    />
                     <TextField
                       id="email"
                       type="email"
@@ -127,40 +102,34 @@ const Page = () => {
                       onBlur={handleBlur}
                       value={values.email}
                       label="Email"
-                      className="w-full"
                       variant="standard"
                       sx={textFieldStyles}
                       fullWidth
                     />
                   </div>
-                  <div className="mt-2">
-                    {errors.email && touched.email && (
-                      <div className="text-red-600 text-sm">{errors.email}</div>
-                    )}
-                  </div>
+                  {errors.email && touched.email && (
+                    <div className="text-red-600 text-sm mt-2">
+                      {errors.email}
+                    </div>
+                  )}
                 </div>
 
                 <div>
                   <div className="flex items-center gap-2">
-                    <div className="mt-3">
-                      <span>
-                        <Image
-                          src="/pass.png"
-                          alt="email"
-                          width={20}
-                          height={20}
-                        />
-                      </span>
-                    </div>
+                    <Image
+                      src="/pass.png"
+                      alt="Password Icon"
+                      width={20}
+                      height={20}
+                    />
                     <TextField
-                      id="standard-basic"
+                      id="password"
                       type={eye ? "password" : "text"}
                       onChange={handleChange}
                       name="password"
                       onBlur={handleBlur}
                       value={values.password}
                       label="Password"
-                      className="w-full"
                       variant="standard"
                       sx={textFieldStyles}
                       fullWidth
@@ -177,24 +146,21 @@ const Page = () => {
                       }}
                     />
                   </div>
-                  <div className="mt-2">
-                    {errors.password && touched.password && (
-                      <div className="text-red-600 text-sm">
-                        {errors.password}
-                      </div>
-                    )}
-                  </div>
+                  {errors.password && touched.password && (
+                    <div className="text-red-600 text-sm mt-2">
+                      {errors.password}
+                    </div>
+                  )}
                 </div>
 
-                <div className="button mt-3">
-                  <button className="cursor-pointer w-full p-3 duration-200 hover:bg-[#ecdb76] font-bold bg-[#FCC827]">
-                    Login
-                  </button>
-                </div>
-                <div className="forget text-center mt-4">
-                  <Link href="/forgot" className="text-center">
-                    Forgot Password?
-                  </Link>
+                <button
+                  type="submit"
+                  className="w-full cursor-pointer p-3 font-bold bg-[#FCC827] hover:bg-[#ecdb76] duration-200"
+                >
+                  Login
+                </button>
+                <div className="text-center mt-4">
+                  <Link href="/forgot">Forgot Password?</Link>
                 </div>
               </div>
             </form>
@@ -203,13 +169,12 @@ const Page = () => {
       </div>
 
       {/* SECOND IMAGE */}
-      <div className="secondimage ">
+      <div className={`${styles.secondImage}`}>
         <Image
           src="/newl3.png"
           width={520}
-          // className={`w-[50%] absolute bottom-0 right-0 sm:w-[40%] md:w-[35%] lg:w-[35%] ${styles.logoImage}`}
-          className={`${styles.logoImage} absolute w-[35%] bottom-0 right-0 sm:w-[40%] md:w-[45%] lg:w-[35%]`}
           height={350}
+          className={`${styles.logoImage}`}
           alt="l3"
         />
       </div>
